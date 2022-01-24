afcf130e07fe96f43625f4521cf6dcb5047e6a33
srlopez <srlopezh@gmail.com>
2022-01-24 18:55:37 +0100

    Refactoring gid

--- Program.cs
+++ Program.cs
@@ -16,39 +16,42 @@ namespace BackProcess
 
         static async Task Main(string[] args)
         {
-            try {
+            try
+            {
                 //intervalo = 1000 * Int16.Parse(args[0]);
                 intervalo = 1000 * Int16.Parse(
                     Environment.GetEnvironmentVariable("SEGUNDOS")
                     );
-            } catch  {  }
-Console.WriteLine("Main Empezamos");
-            ShowThreadInfo("Aplicación");
+            }
+            catch { }
+            string gid = Guid.NewGuid().ToString().Split("-")[0];
+            Console.WriteLine($"{gid}: Main Empezamos");
+            ShowThreadInfo("Aplicación", gid);
             while (count < 5)
             {
-                var t = Task.Run(() => ShowThreadInfo($"Tarea {++count}"));
+                var t = Task.Run(() => ShowThreadInfo($"Tarea {++count}", gid));
                 // Esperamos que acabe el hilo (si interesa)
                 t.Wait();
                 // Esperamos el intervalo
                 System.Threading.Thread.Sleep(intervalo);
             }
-Console.WriteLine("Main Acabamos");
+            Console.WriteLine($"{gid}: Main Acabamos");
 
         }
 
         // Hilo que realiza nuestra tarea
-        async static void ShowThreadInfo(String s)
+        async static void ShowThreadInfo(String s, String gid)
         {
             var id = Thread.CurrentThread.ManagedThreadId;
-            Console.WriteLine($"{s} thread ID: {id}");
-            await TareaAsincrona(3000, id );
+            Console.WriteLine($"{gid}: {s} thread ID: {id}");
+            await TareaAsincrona(3000, id, gid);
         }
 
-        static async Task TareaAsincrona(int number, int id)
+        static async Task TareaAsincrona(int number, int id, string gid)
         {
-            Console.WriteLine($"Asincrona begin {id}");
+            Console.WriteLine($"{gid}: Asincrona begin {id},  {DateTime.Now}","es-ES");
             await Task.Delay(number);
-            Console.WriteLine($"Asincrona Fin {id}");
+            Console.WriteLine($"{gid}: Asincrona Fin {id}");
         }
     }
 }
